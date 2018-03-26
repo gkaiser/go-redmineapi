40678f85e3c4da9c190ba5fb5242b6fef153467e
Guy Kaiser <grkaiser@gmail.com>
2018-03-25 21:02:54 -0400

    Work work work

--- redmineutil.go
+++ redmineutil.go
@@ -11,37 +11,84 @@ import (
 var usersCollection RedmineUsersCollection
 
 // GetIssues returns issues for a given Redmine user
-func GetIssues(key string, user string, limit int) ([]Issue, error) {
+func GetIssues(key string, user string, limit int) (ret []Issue, retErr error) {
 	log.Printf("redmineutil.GetIssues - Getting issues...")
+	client := &http.Client{}
 
 	if usersCollection.TotalCount == int64(0) {
 		log.Printf("redmineutil.GetIssues -   Getting users first...")
-
-		client := &http.Client{}
-
-		req, err := http.NewRequest("GET", "https://vault.softwaresysinc.net/redmine/users.json", nil)
-		if err != nil {
-			et := fmt.Sprintf("redmineutil.GetIssues failed to create a request to get users: %s", err.Error())
-			log.Printf(et)
-			return nil, errors.New(et)
-		}
-
-		req.Header.Add("User-Agent", "SSIbot/0.1")
-		req.Header.Add("X-Redmine-API-Key", key)
-
-		resp, err := client.Do(req)
-		if err != nil {
-			et := fmt.Sprintf("redmineutil.GetIssues failed to get users response: %s", err.Error())
-			log.Printf(et)
-			return nil, errors.New(et)
-		}
-
-		json.NewDecoder(resp.Body).Decode(&usersCollection)
-
+		GetUsers(key)
 		log.Printf("redmineutil.GetIssues -   Got %d user records...", len(usersCollection.Users))
 	}
 
-	return make([]Issue, 0), nil
+	// find the ID for the user we're getting issues for
+	userId := int64(-1)
+	for _, usr := range usersCollection.Users {
+		if usr.Firstname == user || usr.Lastname == user {
+			userId = usr.ID
+			break
+		}
+	}
+
+	if userId == -1 {
+		et := fmt.Sprintf("redmine.GetIssues -   Couldn't find a user record for \"%s\".", user)
+		log.Printf(et)
+		return nil, errors.New(et)
+	}
+
+	issuesUrl := fmt.Sprintf("https://vault.softwaresysinc.net/redmine/issues.json?assigned_to_id=%d", userId)
+
+	req, err := http.NewRequest("GET", issuesUrl, nil)
+	if err != nil {
+		et := fmt.Sprintf("redmineutil.GetIssues failed to create a request to get issues: %s", err.Error())
+		log.Printf(et)
+		return nil, errors.New(et)
+	}
+
+	req.Header.Add("User-Agent", "SSIbot/0.1")
+	req.Header.Add("X-Redmine-API-Key", key)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		et := fmt.Sprintf("redmineutil.GetIssues failed to get issues response: %s", err.Error())
+		log.Printf(et)
+		return nil, errors.New(et)
+	}
+
+	var issuesCollection RedmineIssuesCollection
+	json.NewDecoder(resp.Body).Decode(&issuesCollection)
+
+	log.Printf("redmineutil.GetIssues -   Got %d total issue records...", len(issuesCollection.Issues))
+	for _, issue := range issuesCollection.Issues {
+		if issue.AssignedTo.ID == userId {
+			ret = append(ret, issue)
+		}
+	}
+
+	log.Printf("redmineutil.GetIssues -   Pruned the list of issue records down to %d...", len(ret))
+
+	return ret, nil
+}
+
+func GetUsers(user string) {
+	req, err := http.NewRequest("GET", "https://vault.softwaresysinc.net/redmine/users.json", nil)
+	if err != nil {
+		et := fmt.Sprintf("redmineutil.GetIssues failed to create a request to get users: %s", err.Error())
+		log.Printf(et)
+		return nil, errors.New(et)
+	}
+
+	req.Header.Add("User-Agent", "SSIbot/0.1")
+	req.Header.Add("X-Redmine-API-Key", key)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		et := fmt.Sprintf("redmineutil.GetIssues failed to get users response: %s", err.Error())
+		log.Printf(et)
+		return nil, errors.New(et)
+	}
+
+	json.NewDecoder(resp.Body).Decode(&usersCollection)
 }
 
 type RedmineUsersCollection struct {
@@ -61,7 +108,7 @@ type RedmineUser struct {
 	LastLoginOn string `json:"last_login_on"`
 }
 
-// RedmineIssues struct containing issues
+// RedmineIssuesCollection struct containing issues
 type RedmineIssuesCollection struct {
 	Issues     []Issue `json:"issues"`
 	TotalCount int64   `json:"total_count"`
